90f77fcfe2c5c3cff689564206f467fd268f4b68
Jeyson Flores <68255757+JeysonFlores@users.noreply.github.com>
2020-08-25 22:38:15 -0600

    Add files via upload

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,6 @@
 from __main__ import app, db, ma, emp_schema, emps_schema, client_schema, clients_schema, prod_schema, prods_schema, user_schema, Employees, Clients, Products, Users
 from flask import jsonify, request, make_response
+from flask_restful import abort
 from functools import wraps
 import jwt
 import datetime
@@ -12,23 +13,57 @@ def token_required(f):
     def decorated(*args,**kwargs):
         token = request.args.get('token')
         if not token:
-            return jsonify({"error": "1", "message":"token is missing"}), 403
+            #return jsonify({"error": "1", "message":"token is missing"}), 403
+            abort(403)
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'])
         except:
-            return jsonify({"error": "1", "message": "token is invalid"}), 403
+            #return jsonify({"error": "1", "message": "token is invalid"}), 403
+            abort(403)
         return f(*args,**kwargs)
     return decorated
 
+@app.errorhandler(400)
+def bad_request(e):
+    return jsonify({"message": "Invalid request"})
+
+@app.errorhandler(403)
+def unauthorized_request(e):
+    return jsonify({"message": "Unauthorized request"})
+
+@app.errorhandler(404)
+def unknown_route(e):
+    return jsonify({"message": "Invalid route"})
+
+@app.errorhandler(405)
+def invalid_method(e):
+    return jsonify({"message": "Invalid method"})
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    try:
+        name = request.json['name']
+        password = request.json['password']
+        nickname = request.json['nickname']
+        new_user = Users(name,password,nickname)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"error": "0", "message": "user registered"})
+    except:
+        abort(400)
+
 @app.route('/login')
 def login():
-    user_name = request.json['name']
-    user_password = request.json['password']
-    user = Users.query.filter_by(name=user_name,password=user_password).first()
-    if user is not None:
-        token = jwt.encode({"username": user.id,"password": user.password, "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=50)}, app.config['SECRET_KEY'])
-        return jsonify({"token": token.decode('UTF-8')})
-    return make_response(jsonify({"error": "1", "message": "Login Failed"}))
+    try:
+        user_name = request.json['name']
+        user_password = request.json['password']
+        user = Users.query.filter_by(name=user_name,password=user_password).first()
+        if user is not None:
+            token = jwt.encode({"username": user.id,"password": user.password, "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=50)}, app.config['SECRET_KEY'])
+            return jsonify({"token": token.decode('UTF-8')})
+        abort(400)
+    except:
+        abort(400)
 
 @app.route('/about')
 @token_required
@@ -61,29 +96,35 @@ def get_employee(id):
 @app.route('/employees', methods=['POST'])
 @token_required
 def add_employee():
-    name = request.json['name']
-    email = request.json['email']
-    telephone = request.json['telephone']
-    job = request.json['job']
-    gender = request.json['gender']
-    new_e = Employees(name,email,telephone,job,gender)
-    db.session.add(new_e)
-    db.session.commit()
-    return emp_schema.jsonify(new_e)
+    try:
+        name = request.json['name']
+        email = request.json['email']
+        telephone = request.json['telephone']
+        job = request.json['job']
+        gender = request.json['gender']
+        new_e = Employees(name,email,telephone,job,gender)
+        db.session.add(new_e)
+        db.session.commit()
+        return emp_schema.jsonify(new_e)
+    except:
+        abort(400)
 
 @app.route('/employees/<id>', methods=['PUT'])
 @token_required
 def mod_employee(id):
-    emp = Employees.query.get(id)
-    if emp is not None:
-        emp.name = request.json['name']
-        emp.email = request.json['email']
-        emp.telephone = request.json['telephone']
-        emp.job = request.json['job']
-        emp.gender = request.json['gender']
-        db.session.commit()
-        return emp_schema.jsonify(emp)
-    return jsonify({"error": "1", "message": "requested employee doesn't exist"})
+    try:
+        emp = Employees.query.get(id)
+        if emp is not None:
+            emp.name = request.json['name']
+            emp.email = request.json['email']
+            emp.telephone = request.json['telephone']
+            emp.job = request.json['job']
+            emp.gender = request.json['gender']
+            db.session.commit()
+            return emp_schema.jsonify(emp)
+        return jsonify({"error": "1", "message": "requested employee doesn't exist"})
+    except:
+        abort(400)
 
 @app.route('/clients', methods=['GET'])
 @token_required
@@ -105,27 +146,33 @@ def get_client(id):
 @app.route('/clients', methods=['POST'])
 @token_required
 def add_client():
-    name = request.json['name']
-    address = request.json['address']
-    contact = request.json['contact']
-    telephone = request.json['telephone']
-    new_c = Clients(name,address,contact,telephone)
-    db.session.add(new_c)
-    db.session.commit()
-    return client_schema.jsonify(new_c)
+    try:
+        name = request.json['name']
+        address = request.json['address']
+        contact = request.json['contact']
+        telephone = request.json['telephone']
+        new_c = Clients(name,address,contact,telephone)
+        db.session.add(new_c)
+        db.session.commit()
+        return client_schema.jsonify(new_c)
+    except:
+        abort(400)
 
 @app.route('/clients/<id>', methods=['PUT'])
 @token_required
 def mod_client(id):
-    client = Clients.query.get(id)
-    if client is not None:
-        client.name = request.json['name']
-        client.address = request.json['address']
-        client.contact = request.json['contact']
-        client.telephone = request.json['telephone']
-        db.session.commit()
-        return client_schema.jsonify(client)
-    return jsonify({"error": "1", "message": "requested client doesn't exist"})
+    try:
+        client = Clients.query.get(id)
+        if client is not None:
+            client.name = request.json['name']
+            client.address = request.json['address']
+            client.contact = request.json['contact']
+            client.telephone = request.json['telephone']
+            db.session.commit()
+            return client_schema.jsonify(client)
+        return jsonify({"error": "1", "message": "requested client doesn't exist"})
+    except:
+        abort(400)
 
 @app.route('/products', methods=['GET'])
 @token_required
@@ -147,24 +194,30 @@ def get_product(id):
 @app.route('/products', methods=['POST'])
 @token_required
 def add_product():
-    name = request.json['name']
-    quantity = request.json['quantity']
-    price = request.json['price']
-    description = request.json['description']
-    new_p = Products(name,quantity,price,description)
-    db.session.add(new_p)
-    db.session.commit()
-    return prod_schema.jsonify(new_p)
+    try:
+        name = request.json['name']
+        quantity = request.json['quantity']
+        price = request.json['price']
+        description = request.json['description']
+        new_p = Products(name,quantity,price,description)
+        db.session.add(new_p)
+        db.session.commit()
+        return prod_schema.jsonify(new_p)
+    except:
+        abort(400)
 
 @app.route('/products/<id>', methods=['PUT'])
 @token_required
 def mod_prod(id):
-    prod = Products.query.get(id)
-    if prod is not None:
-        prod.name = request.json['name']
-        prod.quantity = request.json['quantity']
-        prod.price = request.json['price']
-        prod.description = request.json['description']
-        db.session.commit()
-        return prod_schema.jsonify(prod)
-    return jsonify({"error": "1", "message": "requested product doesn't exist"})
+    try:
+        prod = Products.query.get(id)
+        if prod is not None:
+            prod.name = request.json['name']
+            prod.quantity = request.json['quantity']
+            prod.price = request.json['price']
+            prod.description = request.json['description']
+            db.session.commit()
+            return prod_schema.jsonify(prod)
+        return jsonify({"error": "1", "message": "requested product doesn't exist"})
+    except:
+        abort(400)
